019404cce7863db5b6a5e9c4eee1a1d199f57e09
Esteban Borai <estebanborai@gmail.com>
2020-01-26 20:22:07 -0300

    Update dependencies. Improve webpack setup

---
+++ src/index.tsx
@@ -0,0 +1,8 @@
+import * as React from 'react';
+import ReactDOM from 'react-dom';
+import './style.scss';
+
+ReactDOM.render(
+  <h1>Testing</h1>,
+  document.getElementById('root')
+);
--- webpack.config.js
+++ webpack.config.js
@@ -1,78 +1,103 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const { HotModuleReplacementPlugin } = require('webpack');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const DefinePlugin = require('webpack').DefinePlugin;
+const HotModuleReplacementPlugin = require('webpack').HotModuleReplacementPlugin;
 
-module.exports = {
-	entry: './src/index.jsx',
-	output: {
-		filename: 'index.js',
-		path: path.resolve(__dirname, 'bundle'),
-	},
-	module: {
-		rules: [
-			{
-				test: /\.(js|jsx)$/,
-				exclude: /node_modules\/(?!react-intl|intl-messageformat|intl-messageformat-parser)/,
-				use: {
-					loader: 'babel-loader',
-				},
-			},
-			{
-				test: /\.css$/,
-				use: [
-					'style-loader',
-					'css-loader',
-				],
-			},
-			{
-				test: /\.s[ac]ss$/i,
-				use: [
-					'style-loader',
-					'css-loader',
-					'sass-loader',
-				],
-			},
-			{
-				test: /\.(png|jpg|gif|svg)$/,
-				use: [
-					{
-						loader: 'file-loader',
-						options: {
-							name: 'assets/[name].[ext]',
-						},
-					},
-				],
-			},
-			{
-				test: /\.(woff|woff2|eot|ttf|otf)$/,
-				loader: 'file-loader',
-				options: {
-					name: 'assets/[name].[ext]',
-				},
-			},
-		],
-	},
-	devServer: {
-		contentBase: path.join(__dirname, 'src'),
-		compress: true,
-		port: 8080,
-		historyApiFallback: true,
-	},
-	plugins: [
-		new HtmlWebpackPlugin({
-			template: 'public/index.html',
-		}),
-		new HotModuleReplacementPlugin(),
-		new CleanWebpackPlugin(),
-	],
-	resolve: {
-		extensions: ['.js', '.jsx'],
-		alias: {
-			components: path.resolve(__dirname, 'src/components'),
-			pages: path.resolve(__dirname, 'src/pages'),
-			sections: path.resolve(__dirname, 'src/sections'),
-			styles: path.resolve(__dirname, 'src/styles'),
-		},
-	},
+module.exports = (env, args) => {
+  const config = {};
+  const isDevelopment = env === 'development';
+
+  if (isDevelopment) {
+    console.log('Running on Development Mode');
+  }
+
+  config.devtool = isDevelopment && 'source-map';
+
+  // Entry File
+  config.entry = './src/index.tsx';
+
+  // Bundle target file
+  config.output = {
+    filename: 'index.js',
+  };
+
+  // Modules configuration
+  config.module = {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: 'ts-loader',
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: 'style.css',
+            }
+          },
+          {
+            loader: 'extract-loader'
+          },
+          {
+            loader: 'css-loader?-url'
+          },
+          {
+            loader: 'postcss-loader'
+          },
+          {
+            loader: 'sass-loader'
+          }
+        ],
+      },
+    ]
+  };
+
+  // Webpack Dev Server
+  config.devServer = {
+    contentBase: path.join(__dirname, 'src'),
+    hot: true,
+    inline: true,
+    compress: true,
+    port: 8080,
+    historyApiFallback: true,
+    watchOptions: {
+      ignored: /node_modules/,
+      aggregateTimeout: 1500,
+    },
+    stats: {
+      colors: true,
+      hash: false,
+      version: false,
+      timings: false,
+      assets: false,
+      chunks: false,
+      modules: false,
+      reasons: false,
+      children: false,
+      source: false,
+      errors: true,
+      errorDetails: true,
+      warnings: true,
+      publicPath: false
+    }
+  };
+
+  config.plugins = [
+    new HtmlWebpackPlugin({
+      title: 'Esteban Borai | GitHub Page',
+      template: './public/index.ejs',
+    }),
+    new HotModuleReplacementPlugin(),
+  ];
+
+  config.resolve = {
+    extensions: ['.ts', '.tsx', '.js'],
+    alias: {
+      components: path.resolve(__dirname, 'src/components'),
+    }
+  };
+
+  return config;
 };
