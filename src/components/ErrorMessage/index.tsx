151f512c0a95a723f3bff4f9d8dc1f7300754f13
Esteban Borai <estebanborai@gmail.com>
2020-03-01 23:28:09 -0300

    Minor work on error handling

--- src/components/ErrorMessage/index.tsx
+++ src/components/ErrorMessage/index.tsx
@@ -1,11 +1,19 @@
 import React from 'react';
+import './error-message.scss';
 import { ErrorContext } from 'context';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faWifi, faBomb } from '@fortawesome/free-solid-svg-icons';
 
 const ErrorMessage = (): JSX.Element => {
   const errorContext = React.useContext(ErrorContext);
 
   return (
-  <h1>{JSON.stringify(errorContext)}</h1>
+    <div id="error-message" className="fatal">
+      <span className="icon-container">
+        <FontAwesomeIcon icon={faBomb} />
+      </span>
+      <span>Something is wrong!</span>
+    </div>
   )
 }
 
