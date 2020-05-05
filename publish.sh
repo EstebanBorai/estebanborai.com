042a4503efb1159c98cbf0df362fd485928e983f
Esteban Borai <estebanborai@gmail.com>
2020-05-05 02:50:22 -0300

    Create publish.sh

---
+++ publish.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+RED_COLOR='\033[0;31m'
+GREEN_COLOR='\033[0;32m'
+PURPLE_COLOR='\033[0;35m'
+DEFAULT_COLOR='\033[0m'
+
+function log() {
+  if [[ $2 = "error" ]]; then
+    printf "${PURPLE_COLOR}>> ${RED_COLOR} (!) $1${DEFAULT_COLOR}\n\n"
+  else
+    printf "${PURPLE_COLOR}>> ${GREEN_COLOR} $1${DEFAULT_COLOR}\n\n"
+  fi
+}
+
+# Commits the current state of "develop" branch
+# builds and push.
+# Then checkouts to "master" branch and pushes the build
+commit_and_publish () {
+  echo "Commit message:"
+  read COMMIT_MESSAGE
+
+  log "Building estebanborai/estebanborai.github.io"
+  npm run build
+
+  log "Copying files to external `bundle` directory"
+  mkdir  ~/Desktop/esteban-borai-github-io-bundle-temporal-dir/
+  cp -R ./dist/** ~/Desktop/esteban-borai-github-io-bundle-temporal-dir/
+
+  log "Checking out the current branch"
+  git add .
+  git commit -m "[🤖] $COMMIT_MESSAGE"
+  git push origin develop
+
+  log "Copying new bundle files"
+  git checkout master
+
+  cp -rf ~/Desktop/esteban-borai-github-io-bundle-temporal-dir/** ./
+  
+  log "Publshing to GitHub Pages"
+  git add .
+  git commit -m "[🤖] $COMMIT_MESSAGE"
+  git push origin master
+  git checkout develop
+
+  log "Removing temporal directory \"~/Desktop/esteban-borai-github-io-bundle-temporal-dir/\""
+  rm -rf ~/Desktop/esteban-borai-github-io-bundle-temporal-dir/
+
+  log "Build finished!"
+}
+
+commit_and_publish
