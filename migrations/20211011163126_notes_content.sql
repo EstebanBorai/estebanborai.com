4cf40a619f9edfb56ecc12ec7480cba3f5c9407e
Esteban Borai <estebanborai@gmail.com>
2021-10-15 00:27:02 -0300

    chore(ci): setup CI workflows for server

--- migrations/20211011163126_notes_content.sql
+++ migrations/20211011163126_notes_content.sql
@@ -1,7 +1,7 @@
 -- Add migration script here
 CREATE TABLE IF NOT EXISTS notes_content (
   id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
-  content TEXT,
+  content TEXT NOT NULL,
   notes_metadata_id UUID UNIQUE NOT NULL,
   created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
   updated_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
