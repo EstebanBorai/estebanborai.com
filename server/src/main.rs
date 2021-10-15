9c5dbd16ffbba8fc2f2025c5ffaac4c92fe686ff
Esteban Borai <estebanborai@gmail.com>
2021-10-15 01:13:16 -0300

    feat(server): use `PORT` environment variable

--- server/src/main.rs
+++ server/src/main.rs
@@ -2,6 +2,9 @@ mod error;
 mod routes;
 mod services;
 
+use rocket::Config;
+use std::env;
+
 #[macro_use]
 extern crate rocket;
 
@@ -15,9 +18,17 @@ async fn rocket() -> _ {
         dotenv().ok().expect("Failed to load dotenv");
     }
 
+    let port = env::var("PORT")
+        .unwrap_or(String::from("7878"))
+        .parse::<u16>()
+        .expect("Not a valid u16 value for PORT environment variable");
+    let config = Config {
+        port,
+        ..Config::debug_default()
+    };
     let services = services::Services::new().await;
 
-    rocket::build().manage(services).mount(
+    rocket::custom(&config).manage(services).mount(
         "/api/v1",
         routes![routes::notes::index, routes::notes::find_by_slug],
     )
