b93a9938c0cbfc4ba7123425b1c16393b98495c2
Esteban Borai <estebanborai@gmail.com>
2022-01-02 18:54:52 -0300

    feat: impl homepage

---
+++ client/src/lib/components/Footer.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+  const currentYear = new Date().getFullYear();
+</script>
+
+<footer>
+  <small>Made with 🧉 and ❤️ by Esteban Borai &copy; 2020 - {currentYear}</small>
+</footer>
--- client/src/routes/__layout.svelte
+++ client/src/routes/__layout.svelte
@@ -1,8 +1,16 @@
 <script lang="ts">
-	import Header from '$lib/components/Header.svelte';
+  import Header from '$lib/components/Header.svelte';
+	import Footer from '$lib/components/Footer.svelte';
 
 	import '$lib/styles/global.css';
 </script>
 
+<svelte:head>
+  <title>Esteban Borai</title>
+</svelte:head>
+
 <Header />
-<slot />
+<main>
+  <slot />
+</main>
+<Footer />
