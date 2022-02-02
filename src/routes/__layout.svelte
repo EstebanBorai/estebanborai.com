426907cf84898088749e968824ba47ae78d30366
Esteban Borai <estebanborai@gmail.com>
2022-02-01 23:30:10 -0300

    feat: setup sentry for browser

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,8 +1,22 @@
 <script lang="ts">
+	import { init } from '@sentry/browser';
+	import { Integrations } from '@sentry/tracing';
+	import { onMount } from 'svelte';
+
 	import Header from '$lib/components/Header.svelte';
 	import Footer from '$lib/components/Footer.svelte';
 
 	import '$lib/styles/global.css';
+
+	onMount(async () => {
+		init({
+			dsn: 'https://4f6bf36480d54546a35b1b7740fa7fe5@o1131101.ingest.sentry.io/6179130',
+			enabled: true,
+			environment: import.meta.env.MODE,
+			integrations: [new Integrations.BrowserTracing()],
+			tracesSampleRate: 0.3
+		});
+	});
 </script>
 
 <svelte:head>
