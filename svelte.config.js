64238e45d7fe182ccb6495d38d8bee94fa075bf1
Esteban Borai <estebanborai@gmail.com>
2022-07-23 19:29:51 -0400

    fix: use config from template

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,52 @@
+import forms from '@tailwindcss/forms';
+import typography from '@tailwindcss/typography';
 import adapter from '@sveltejs/adapter-cloudflare';
+import autoprefixer from 'autoprefixer';
+import { mdsvex } from 'mdsvex';
 import preprocess from 'svelte-preprocess';
+import tailwindcss from 'tailwindcss';
 
 /** @type {import('@sveltejs/kit').Config} */
 export default {
-  preprocess: preprocess({
-    sourceMap: true,
-  }),
+  extensions: ['.svelte', '.svx'],
+  preprocess: [
+    preprocess({
+      sourceMap: true,
+      postcss: {
+        plugins: [
+          tailwindcss({
+            darkMode: 'class',
+            content: ['./src/**/*.{html,js,svelte,ts}'],
+            theme: {
+              extend: {
+                colors: {
+                  light: {
+                    background: '#F6F5F1',
+                    'background-alt': '#FEFEFE',
+                  },
+                  dark: {
+                    background: '#222222',
+                    'background-alt': '#2C2C2E',
+                  },
+                },
+                fontFamily: {
+                  body: ['Inter', 'sans-serif'],
+                },
+              },
+            },
+            plugins: [typography, forms],
+            variants: {
+              extend: {},
+            },
+          }),
+          autoprefixer,
+        ],
+      },
+    }),
+    mdsvex({
+      layout: './src/lib/modules/notes/components/Layout.svelte',
+    }),
+  ],
   kit: {
     adapter: adapter(),
   },
--- vite.config.js
+++ vite.config.js
@@ -1,58 +1,14 @@
 import { sveltekit } from '@sveltejs/kit/vite';
-import forms from '@tailwindcss/forms';
-import typography from '@tailwindcss/typography';
-import autoprefixer from 'autoprefixer';
-import { mdsvex } from 'mdsvex';
-import tailwindcss from 'tailwindcss';
 
 /** @type {import('vite').UserConfig} */
 const config = {
   build: {
     sourcemap: true,
   },
-  css: {
-    postcss: {
-      plugins: [
-        tailwindcss({
-          darkMode: 'class',
-          mode: 'jit',
-          content: ['./src/**/*.{html,js,svelte,ts}'],
-          theme: {
-            extend: {
-              colors: {
-                light: {
-                  background: '#F6F5F1',
-                  'background-alt': '#FEFEFE',
-                },
-                dark: {
-                  background: '#222222',
-                  'background-alt': '#2C2C2E',
-                },
-              },
-              fontFamily: {
-                body: ['Inter', 'sans-serif'],
-              },
-            },
-          },
-          plugins: [typography, forms],
-        }),
-        autoprefixer,
-      ],
-    },
-  },
   server: {
     port: 3000,
   },
-  // an array of file extensions that should be treated as Svelte components
-  extensions: ['.svelte', '.svx'],
   assetsInclude: ['**/*.css', '**/*.svx'],
-  // Consult https://github.com/sveltejs/svelte-preprocess
-  // for more information about preprocessors
-  preprocess: [
-    mdsvex({
-      layout: './src/lib/modules/notes/components/Layout.svelte',
-    }),
-  ],
   plugins: [sveltekit()],
 };
 
